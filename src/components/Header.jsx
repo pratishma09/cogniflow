4528318a94b93ff5de49ffefda541bac433c2a67
pratishma09 <velv9tkiss3s@gmail.com>
2024-10-21 19:45:23 +0545

    complete(navbar)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,87 @@
-import {useLocation} from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 import { brainwaveSymbol } from "../assets";
 import { navigation } from "../constants";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { useState } from "react";
+import { HambugerMenu } from "./design/Header";
 
 const Header = () => {
-const pathname=useLocation();
+  const pathname = useLocation();
+
+  const [openNavigation, setOpenNavigation] = useState(true);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      //mobilemenu
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
 
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90  border-b border-n-6 lg:bg-n-8/90 backdrop-blur-sm">
+    <div
+      className={`fixed top-0 z-50 left-0 w-full bg-n-8/90  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-      <a className="flex items-center gap-2 w-[12rem] xl:mr-8" href="#hero">
-        <img src={brainwaveSymbol} width={30} alt="" /><p className="text-lg font-medium ">CogniFlow</p>
-      </a>
-      <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent ">
-        <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-    {navigation.map((item)=>(
-        <a key={item.id} href={item.url} className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1
-        ${item.onlyMobile?"lg:hidden":""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url===pathname.hash?'z-2 lg:text-n-1':'lg:text-n-1/50'} lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
-            {item.title}
+        <a className="flex items-center gap-2 w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwaveSymbol} width={30} alt="" />
+          <p className="text-lg font-medium ">CogniFlow</p>
         </a>
-    ))}
-        </div>
-      </nav>
+        <nav
+          className={` ${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent `}
+        >
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                onClick={handleClick}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1
+        ${
+          item.onlyMobile ? "lg:hidden" : ""
+        } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+          <HambugerMenu />
+        </nav>
+
+        <Button className="hidden lg:flex mr-10" href="#login">
+          Sign In
+        </Button>
+        <Button className="ml-auto lg:hidden" onClick={toggleNavigation}>
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New account
+        </a>
+        
       </div>
-     
     </div>
   );
 };
---
+++ src/components/Section.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Section = (
+    {
+        className,
+        id,
+        crosses,
+        crossesOffset,
+        customPaddings,
+    }
+) => {
+  return (
+    <div>Section</div>
+  )
+}
+
+export default Section
