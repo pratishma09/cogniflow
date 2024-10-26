143a6d25571bdd19d4a49f7e60b859ba96f54cc7
pratishma09 <velv9tkiss3s@gmail.com>
2024-10-26 20:50:01 +0545

    add(comments)

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,7 +4,9 @@ import Heading from './Heading'
 
 const Benefits = () => {
   return (
+
     <Section id="features">
+      <div></div>
         <div className='container relative z-2'>
             <Heading className="md:max-w-md lg:max-w-2xl" title="Chat Smarter, Not Harder with CogniFlow"/></div>Benefits</Section>
   )
