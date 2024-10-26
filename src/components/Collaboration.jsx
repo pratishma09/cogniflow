919e67d0bab5585cbecdd1b6ff7600debc598ad7
pratishma09 <velv9tkiss3s@gmail.com>
2024-10-26 21:51:42 +0545

    feat(collaboration)

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Section from './Section'
+import { collabContent } from '../constants'
+
+const Collaboration = () => {
+  return (
+    <Section crosses>
+      <div className='container lg:flex'>
+        <div className='max-w-[25rem]'>
+          <h2 className='h2 mb-4 md:mb-8'>AI Chat App for seamless collaboration</h2>
+          <ul className='max-w-[22rem] mb-10 md:mb-14'>
+            {collabContent.map((item)=>(
+              <li className='mb-3 py-3' key={item.id}>
+
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Section>
+  )
+}
+
+export default Collaboration
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Heading = ({className, title}) => {
+const Heading = ({ className, title }) => {
   return (
-    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>{title && <h2 className="h2">{title}</h2>}</div>
-  )
-}
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+      {title && <h2 className="h2">{title}</h2>}
+    </div>
+  );
+};
 
-export default Heading
+export default Heading;
